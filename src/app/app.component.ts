e79a70b5f092ed11bdc6c8c6169bc763461ffd14
thalles.melo <thalles.melo@mestria.com.br>
2024-03-12 15:22:55 -0300

    Feito projeto introdutorio da alura sobre angula.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CantainerComponent } from './componentes/cantainer/cantainer.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CantainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPortexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+
+    return this.contatos.filter((contato) => {
+      return contato.nome
+        .toLowerCase()
+        .includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPortexto().filter((contato) =>
+      this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .startsWith(this.removerAcentos(letra).toLowerCase())
+    );
+  }
 }
---
+++ src/app/componentes/cantainer/cantainer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cantainer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cantainer.component.html',
+  styleUrl: './cantainer.component.css',
+})
+export class CantainerComponent {}
